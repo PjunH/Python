0a2e544c9a3668f52630cdd43dd7d1173f3f1e03
PjunH <102010593+PjunH@users.noreply.github.com>
2022-03-28 15:11:22 +0900

    3/28 Python

---
+++ 2.datatype.py
@@ -0,0 +1,36 @@
+print(1)
+print(1.1)
+print(1+1)
+print(2-1)
+print(2*2)
+print(6/2)
+print(pow(3,2))
+import random 
+print(random.random())
+
+# String
+print('hello')
+print("hello")
+print('''
+    Hello
+    World
+''')
+print(len('hello'))  #글자수세기
+
+#List
+
+member = ['Park','duru','taeho']
+print(member[0])
+print(len(member))
+
+import random
+print(random.choice(member))
+
+score = [100,200,300]
+print(sum(score))
+
+#list 데이터
+
+#print ('hello world')
+#name = "hello world"
+#print(f"'{name}".replace("hello world", "GGG"))
